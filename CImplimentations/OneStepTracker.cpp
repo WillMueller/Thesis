5fb36881405a7df57018e0a81a60b84b9411c114
WillemM <will_mueller@live.ca>
2016-02-06 21:51:33 -0500

    started wooo

---
+++ CImplimentations/OneStepTracker.cpp
@@ -0,0 +1,40 @@
+// Region Tracking test code
+
+#include <stdio.h>
+
+class image
+{
+	private:
+		int width;
+		int height;
+		int** bitMap;
+
+	public:
+		
+};
+
+// aquires both images and initalizes the level set u
+void  init(int[][] image1, int[][] image2, float[][] u)
+{
+
+}
+
+// tracks the region and alters the level set u to represent the next region
+void decent(int[][] image1, int[][] image2, float[][] u)
+{
+
+}
+
+// saves a file displaying the subsequent region 
+void show(int[][] image2, float[][] u)
+{
+
+}
+
+int main()
+{
+	int[][] image1
+	int[][]
+
+
+}
