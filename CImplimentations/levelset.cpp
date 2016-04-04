1cc6a8332fbe59243179d3490bb58a38f80979d9
Zachary Hervieux-Moore <zachary.moore.1993@gmail.com>
2016-04-03 20:42:33 -0400

    Most recent versions of the fiels

--- CImplimentations/levelset.cpp
+++ CImplimentations/levelset.cpp
@@ -10,9 +10,9 @@ LevelSet::LevelSet()
 }
 
 LevelSet::LevelSet(QImage image)
-{
+{    
     m_image_master = image;
-    m_image = m_image_master;
+    m_image = image;
 
     // Initialize derivative arrays
 
@@ -40,6 +40,19 @@ LevelSet::LevelSet(QImage image)
 
     m_K.resize(m_width, std::vector<float>(m_height, 0.0));
     m_F.resize(m_width, std::vector<float>(m_height, 0.0));
+
+    // Initialize parameters to 0
+    m_orig_com.resize(2,0.0);
+    m_orig_mean_inside.resize(3,0.0);
+    m_orig_mean_outside.resize(3,0.0);
+    m_orig_variance_inside.resize(3,0.0);
+    m_orig_variance_outside.resize(3,0.0);
+
+    m_com.resize(2,0.0);
+    m_mean_inside.resize(3,0.0);
+    m_mean_outside.resize(3,0.0);
+    m_variance_inside.resize(3,0.0);
+    m_variance_outside.resize(3,0.0);
 }
 
 LevelSet::~LevelSet()
@@ -86,18 +99,22 @@ float LevelSet::descent_func()
 
     float max_F = 0.0;
 
-    float epsilon = 1000.0;
+    // Calculate parameters before the cost_func is called
+    calculate_area();
+    calculate_com();
+    calculate_mean();
+    calculate_variance();
 
     for(int i=1; i < m_width-1; i++)
     {
         for(int j=1; j < m_height-1; j++)
         {
             m_K.at(i).at(j) = curvature(i, j);
-            m_F.at(i).at(j) = cost_func(i, j)/500.0;
+            m_F.at(i).at(j) = cost_func(i, j);
 
-            if(fabs(epsilon*m_K.at(i).at(j) + m_F.at(i).at(j)) > max_F)
+            if(fabs(m_coef_length*m_K.at(i).at(j) + m_F.at(i).at(j)) > max_F)
             {
-                max_F = fabs(epsilon*m_K.at(i).at(j) + m_F.at(i).at(j));
+                max_F = fabs(m_coef_length*m_K.at(i).at(j) + m_F.at(i).at(j));
             }
         }
     }
@@ -113,13 +130,7 @@ float LevelSet::descent_func()
     {
         for(int j=1; j < m_height-1; j++)
         {
-            m_u.at(i).at(j) = m_u_temp.at(i).at(j) + delta_t * (epsilon*m_K.at(i).at(j) * pow(pow(m_disc_grad.at(4).at(i).at(j),2) + pow(m_disc_grad.at(5).at(i).at(j),2),0.5) - (fmax(m_F.at(i).at(j),0)*grad_plus(i,j)+fmin(m_F.at(i).at(j),0)*grad_minus(i,j)));
-
-            // Limit size of u
-            if(fabs(m_u.at(i).at(j)) > 10000)
-            {
-                m_u.at(i).at(j) = m_u.at(i).at(j)/fabs(m_u.at(i).at(j))*10000;
-            }
+            m_u.at(i).at(j) = m_u_temp.at(i).at(j) + delta_t * (m_coef_length*m_K.at(i).at(j) * pow(pow(m_disc_grad.at(4).at(i).at(j),2) + pow(m_disc_grad.at(5).at(i).at(j),2),0.5) - (fmax(m_F.at(i).at(j),0)*grad_plus(i,j)+fmin(m_F.at(i).at(j),0)*grad_minus(i,j)));
         }
     }
 
@@ -250,12 +261,116 @@ float LevelSet::curvature(int i, int j)
     return num / denom;
 }
 
-// For image segmentation functional
+// Overall functional calls sub functionals
 float LevelSet::cost_func(int i, int j)
+{
+    float area_func_val = 0;
+    float mean_inside_func_val = 0;
+    float mean_outside_func_val = 0;
+    float variance_inside_func_val = 0;
+    float variance_outside_func_val = 0;
+    float com_func_val = 0;
+    float pixel_func_val = 0;
+
+    if(m_coef_area != 0){
+        area_func_val = -m_coef_area*area_func();
+    }
+    if(m_coef_mean_inside != 0 ){
+        mean_inside_func_val = -m_coef_mean_inside*mean_inside_func(i,j);
+    }
+    if(m_coef_mean_outside != 0 ){
+        mean_outside_func_val = -m_coef_mean_outside*mean_outside_func(i,j);
+    }
+    if(m_coef_variance_inside != 0){
+        variance_inside_func_val = -m_coef_variance_inside*variance_inside_func(i,j);
+    }
+    if(m_coef_variance_outside != 0){
+        variance_outside_func_val = -m_coef_variance_outside*variance_outside_func(i,j);
+    }
+    if(m_coef_com != 0){
+        com_func_val = -m_coef_com*com_func(i,j);
+    }
+    if(m_coef_pixel != 0){
+        pixel_func_val = m_coef_pixel*segmentation_func(i,j);
+    }
+
+    return area_func_val + mean_inside_func_val + mean_outside_func_val + variance_inside_func_val + variance_outside_func_val + com_func_val + pixel_func_val;
+}
+
+// Area functional
+float LevelSet::area_func(){
+    return 2*(m_area_inside-m_orig_area);
+}
+
+// Mean functional
+float LevelSet::mean_inside_func(int i, int j){
+    QRgb pixel = m_image.pixel(i-1, j-1);
+
+    float val = 0;
+
+    val += 2*(m_mean_inside.at(0) - m_orig_mean_inside.at(0))*(qRed(pixel)-m_mean_inside.at(0))/m_area_inside;
+    val += 2*(m_mean_inside.at(1) - m_orig_mean_inside.at(1))*(qGreen(pixel)-m_mean_inside.at(1))/m_area_inside;
+    val += 2*(m_mean_inside.at(2) - m_orig_mean_inside.at(2))*(qBlue(pixel)-m_mean_inside.at(2))/m_area_inside;
+
+    return val;
+}
+
+float LevelSet::mean_outside_func(int i, int j){
+    QRgb pixel = m_image.pixel(i-1, j-1);
+
+    float val = 0;
+
+    val += 2*(m_mean_outside.at(0) - m_orig_mean_outside.at(0))*(qRed(pixel)-m_mean_outside.at(0))/m_area_outside;
+    val += 2*(m_mean_outside.at(1) - m_orig_mean_outside.at(1))*(qGreen(pixel)-m_mean_outside.at(1))/m_area_outside;
+    val += 2*(m_mean_outside.at(2) - m_orig_mean_outside.at(2))*(qBlue(pixel)-m_mean_outside.at(2))/m_area_outside;
+
+    return val;
+}
+
+// Variance functional
+
+float LevelSet::variance_inside_func(int i, int j){
+    QRgb pixel = m_image.pixel(i-1, j-1);
+
+    float val = 0;
+
+    val += 2*(m_variance_inside.at(0) - m_orig_variance_inside.at(0))*(qRed(pixel)*qRed(pixel)-m_variance_inside.at(0))/m_area_inside;
+    val += 2*(m_variance_inside.at(1) - m_orig_variance_inside.at(1))*(qGreen(pixel)*qGreen(pixel)-m_variance_inside.at(1))/m_area_inside;
+    val += 2*(m_variance_inside.at(2) - m_orig_variance_inside.at(2))*(qBlue(pixel)*qBlue(pixel)-m_variance_inside.at(2))/m_area_inside;
+
+    return val;
+}
+
+float LevelSet::variance_outside_func(int i, int j){
+    QRgb pixel = m_image.pixel(i-1, j-1);
+
+    float val = 0;
+
+    val += 2*(m_variance_outside.at(0) - m_orig_variance_outside.at(0))*(qRed(pixel)*qRed(pixel)-m_variance_outside.at(0))/m_area_outside;
+    val += 2*(m_variance_outside.at(1) - m_orig_variance_outside.at(1))*(qGreen(pixel)*qGreen(pixel)-m_variance_outside.at(1))/m_area_outside;
+    val += 2*(m_variance_outside.at(2) - m_orig_variance_outside.at(2))*(qBlue(pixel)*qBlue(pixel)-m_variance_outside.at(2))/m_area_outside;
+
+    return val;
+}
+
+// COM functional
+
+float LevelSet::com_func(int i, int j){
+    float val = 0;
+
+    val += 2*(m_com.at(0) - m_orig_com.at(0))*(i - m_com.at(0))/m_area_inside;
+    val += 2*(m_com.at(1) - m_orig_com.at(1))*(j - m_com.at(1))/m_area_inside;
+
+    return val;
+}
+
+// Functional to segment image on color
+float LevelSet::segmentation_func(int i, int j)
 {
     QRgb pixel = m_image.pixel(i-1, j-1);
 
     return pow(qRed(pixel) - 255,2) - pow(qRed(pixel) - 0,2) + pow(qGreen(pixel) - 255,2) - pow(qGreen(pixel) - 0,2) + pow(qBlue(pixel) - 255,2) - pow(qBlue(pixel) - 0,2);
+
 }
 
 void LevelSet::paint_border()
@@ -335,4 +450,144 @@ void LevelSet::mirror_u(){
     m_u.at(0).at(m_height-1) = m_u.at(1).at(m_height-2);
     m_u.at(m_width-1).at(0) = m_u.at(m_width-2).at(1);
     m_u.at(m_width-1).at(m_height-1) = m_u.at(m_width-2).at(m_height-2);
+}
+
+void LevelSet::calculate_parameters(){
+    calculate_area();
+    calculate_com();
+    calculate_mean();
+    calculate_variance();
+
+    m_orig_area = m_area_inside;
+    m_orig_com = m_com;
+    m_orig_mean_inside = m_mean_inside;
+    m_orig_mean_outside = m_mean_outside;
+    m_orig_variance_inside = m_variance_inside;
+    m_orig_variance_outside = m_variance_outside;
+}
+
+void LevelSet::calculate_area(){
+    float area_inside = 0;
+    float area_outside = 0;
+
+    // Calculate area for
+    // inside and outside the region (inside is positive u)
+    for(int i = 0; i < m_width; i++){
+        for(int j = 0; j < m_height; j++){
+            if(m_u.at(i).at(j) >= 0){
+                area_inside += 1;
+            }
+            else{
+                area_outside += 1;
+            }
+        }
+    }
+
+    m_area_inside = area_inside;
+    m_area_outside = area_outside;
+}
+
+void LevelSet::calculate_com(){
+    float area = 0;
+    std::vector<float> com;
+    com.resize(2,0.0);
+
+    // Calculate com for the x,y coordiantes value for
+    // inside the region (inside is positive u)
+    for(int i = 0; i < m_width; i++){
+        for(int j = 0; j < m_height; j++){
+            if(m_u.at(i).at(j) >= 0){
+                area += 1;
+                com.at(0) += i;
+                com.at(1) += j;
+            }
+        }
+    }
+
+    m_com.at(0) = com.at(0)/area;
+    m_com.at(1) = com.at(1)/area;
+}
+
+void LevelSet::calculate_mean(){
+    float area_inside = 0;
+    float area_outside = 0;
+    std::vector<float> mean_inside;
+    std::vector<float> mean_outside;
+    mean_inside.resize(3,0.0);
+    mean_outside.resize(3,0.0);
+
+    // Calculate mean for the RGB value for both
+    // inside and outside the region (inside is positive u)
+    for(int i = 1; i < m_width - 1; i++){
+        for(int j = 1; j < m_height - 1; j++){
+            QRgb pixel = m_image.pixel(i-1, j-1);
+
+            if(m_u.at(i).at(j) >= 0){
+                area_inside += 1;
+                mean_inside.at(0) += qRed(pixel);
+                mean_inside.at(1) += qGreen(pixel);
+                mean_inside.at(2) += qBlue(pixel);
+            }
+            else{
+                area_outside += 1;
+                mean_outside.at(0) += qRed(pixel);
+                mean_outside.at(1) += qGreen(pixel);
+                mean_outside.at(2) += qBlue(pixel);
+            }
+        }
+    }
+
+    m_mean_inside.at(0) = mean_inside.at(0)/area_inside;
+    m_mean_inside.at(1) = mean_inside.at(1)/area_inside;
+    m_mean_inside.at(2) = mean_inside.at(2)/area_inside;
+
+    m_mean_outside.at(0) = mean_outside.at(0)/area_outside;
+    m_mean_outside.at(1) = mean_outside.at(1)/area_outside;
+    m_mean_outside.at(2) = mean_outside.at(2)/area_outside;
+}
+
+void LevelSet::calculate_variance(){
+    float area_inside = 0;
+    float area_outside = 0;
+    std::vector<float> variance_inside;
+    std::vector<float> variance_outside;
+    variance_inside.resize(3,0.0);
+    variance_outside.resize(3,0.0);
+
+    // Calculate mean for the RGB value for both
+    // inside and outside the region (inside is positive u)
+    for(int i = 1; i < m_width - 1; i++){
+        for(int j = 1; j < m_height - 1; j++){
+            QRgb pixel = m_image.pixel(i-1, j-1);
+
+            if(m_u.at(i).at(j) >= 0){
+                area_inside += 1;
+                variance_inside.at(0) += qRed(pixel)*qRed(pixel);
+                variance_inside.at(1) += qGreen(pixel)*qGreen(pixel);
+                variance_inside.at(2) += qBlue(pixel)*qBlue(pixel);
+            }
+            else{
+                area_outside += 1;
+                variance_outside.at(0) += qRed(pixel)*qRed(pixel);
+                variance_outside.at(1) += qGreen(pixel)*qGreen(pixel);
+                variance_outside.at(2) += qBlue(pixel)*qBlue(pixel);
+            }
+        }
+    }
+
+    m_variance_inside.at(0) = variance_inside.at(0)/area_inside;
+    m_variance_inside.at(1) = variance_inside.at(1)/area_inside;
+    m_variance_inside.at(2) = variance_inside.at(2)/area_inside;
+
+    m_variance_outside.at(0) = variance_outside.at(0)/area_outside;
+    m_variance_outside.at(1) = variance_outside.at(1)/area_outside;
+    m_variance_outside.at(2) = variance_outside.at(2)/area_outside;
+}
+
+void LevelSet::unitize_u(){
+    for(int i = 0; i < m_width; i++){
+        for(int j = 0; j < m_height; j++){
+            m_u.at(i).at(j) = m_u.at(i).at(j)/fabs(m_u.at(i).at(j));
+        }
+    }
 }
